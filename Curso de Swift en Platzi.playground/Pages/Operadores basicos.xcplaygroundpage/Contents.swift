94bb7587d24ba7a7197aa556ddf51a5bc5f2811b
Desarrolladores de Kontrol <75795616+KontrolDevs@users.noreply.github.com>
2023-03-02 09:32:01 +0100

    Initial Commit

---
+++ Curso de Swift en Platzi.playground/Pages/Operadores basicos.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+import Foundation
+
+/*Hay 3  tipos de operadores:
+ OPERADORES UNARIOS: son los que actuan sobre un elemeneto que convierten un 5 en un -5
+ OPERADORES BINARIOS: son los que actuan sobre dos elementos, sumar dos numeros, restar dos numeros....
+ OPERADORES TERNARIOS: son los que actuan sobre tres elementos*/
+
+let b = 10
+var a = 5
+a = b
+
+let (x,y) = (1,2)
+
+//let 2 = z
+
+if a == b {
+  print("Los valores de a y b son iguales")
+}
+
+1+2
+5-3
+2*3
+10.0/2.5
+
+"Hello " + "World"
+
+//D/d -> D == d*c+r
+9/4 //c
+9%4 //r
+
+9 == 4*2+1
+
+
+-9%4 // -9 == -2*4 + (-1)
+
+let five = 5
+let minusFive = -five
+let plusFive = -minusFive
+
+let minusSix = -6
+let alsoMinusSix = +minusSix
+
+var number = 5
+number += 3 //number = number + 3
+number -= 2 //number = number - 2
+
+
+/*
+ Operador de asignacion
+= igual
+
+Operador aritmeticos
++ Suma(tambien en cadena)
+- Resta (y negacion)
+* Multiplicacion
+/ Division
+ 
+% Resto de division (solo en enteros)
+
+ 
+ Operadores de acumulacion
+ += Suma acumulada
+ -= Resta acumulada
+ *= Multiplicacion acumulada
+ /= Division acumulada
+ 
+ */
+ 
